21fef5fcae48f5d963620a7ec849061fb7e674b7
quarkusio <no-reply@quarkus.io>
2024-11-16 10:43:34 +0000

    Initial commit

---
+++ src/main/java/de/teampb/soco/llm/example/AppConfig.java
@@ -0,0 +1,8 @@
+package de.teampb.soco.llm.example;
+
+import com.vaadin.flow.component.page.AppShellConfigurator;
+import com.vaadin.flow.theme.Theme;
+
+@Theme("starter-theme")
+public class AppConfig implements AppShellConfigurator {
+}
